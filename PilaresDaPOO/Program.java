6aa3834246cf5012192965be59875bc9c0d2388b
Jhon Santos <kennedyalves871@gmail.com>
2023-09-28 21:03:55 -0300

    Pilares da POO

---
+++ PilaresDaPOO/Program.java
@@ -0,0 +1,15 @@
+package PilaresDaPOO;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		ControleRemoto c = new ControleRemoto();
+		// c.ligar();
+		c.desligar();
+		// c.play();
+		c.maisVolume();
+		c.abrirMenu();
+		c.fecharMenu();
+	}
+}
